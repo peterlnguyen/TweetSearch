f1c607d02bfbfefbf4dee30463fbedd7d2ece96a
Peter <peterlongnguyen@gmail.com>
2013-09-08 00:38:19 -0700

    modularzied keys

--- models/twitterModel.js
+++ models/twitterModel.js
@@ -3,10 +3,10 @@ var twit = require("twit");
 var keys = rek("keys");
 
 var twitClient  = new twit({
-  consumer_key: "ox3svi5a0uN7m99n23tw",
-  consumer_secret: "EHI6FmIdilxqjjvmKDcfJuTWqf2mGWxjw2Vdh3ZEVVc",
-  access_token: "308676043-d5TDPY8Z648XKCQqIIgrFzNQ3cVQxPXxWpgdmE",
-  access_token_secret: "90QQwvC90Z29fOE0Ye7GvuvCTXuygDa6PBgrU3DS8"
+  consumer_key: keys.get_consumer_key,
+  consumer_secret: keys.get_consumer_secret,
+  access_token: keys.get_access_token,
+  access_token_secret: keys.get_access_token_secret
 });
 
 var twitterModel = exports = module.exports = {
@@ -16,7 +16,7 @@ var twitterModel = exports = module.exports = {
     twitClient.get("statuses/user_timeline", 
     { 
       "screen_name": screen_name,
-      "count": 1,
+      "count": 10,
       "include_rts": 1,
     },
     callback);
